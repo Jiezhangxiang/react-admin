8e396cfb2bff68bd0cc2991d1982f628e07fdd0c
Jie Zhangxiang <1778676211.com>
2020-07-08 18:45:02 +0800

    课程管理模块添加新增界面

--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,7 +1,84 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import { Button, Table } from "antd" //导入Button按钮
+import { PlusOutlined, FormOutlined, DeleteOutlined } from "@ant-design/icons" //导入antd中的icon
+import "./index.less"
+// 设置显示table 的列表信息
+const columns = [
+  { title: "分类名称", dataIndex: "name", key: "name" },
 
+  {
+    title: "操作",
+    dataIndex: "",
+    key: "x",
+    width: 200,
+    render: () => {
+      return (
+        <>
+          <Button type="primary" style={{ marginRight: 10 }}>
+            <FormOutlined />
+          </Button>
+          <Button type="danger">
+            <DeleteOutlined />
+          </Button>
+        </>
+      )
+    },
+  },
+]
+
+const data = [
+  {
+    key: 1,
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    description:
+      "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+  },
+  {
+    key: 2,
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    description:
+      "My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.",
+  },
+  {
+    key: 3,
+    name: "Not Expandable",
+    age: 29,
+    address: "Jiangsu No. 1 Lake Park",
+    description: "This not expandable",
+  },
+  {
+    key: 4,
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    description:
+      "My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.",
+  },
+]
 export default class Subject extends Component {
   render() {
-    return <div>Subject...</div>;
+    return (
+      <div className="subject">
+        <Button type="primary" icon={<PlusOutlined />}>
+          新建
+        </Button>
+        <Table
+          className="subjectTable"
+          columns={columns}
+          expandable={{
+            expandedRowRender: (record) => (
+              <p style={{ margin: 0 }}>{record.description}</p>
+            ),
+            rowExpandable: (record) => record.name !== "Not Expandable",
+          }}
+          dataSource={data}
+        />
+        ,
+      </div>
+    )
   }
 }
